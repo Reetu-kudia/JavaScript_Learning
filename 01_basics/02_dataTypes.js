43b564ccca15a0a6398843f064f4a2a0e5dfd516
Reetu Kudia <126332989+Reetu-kudia@users.noreply.github.com>
2024-11-26 05:24:05 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,33 @@
+"use strict"; // treat all JS code as a newer version (used all new syntax)
+
+// alert(3+3) // this will give error bcoz we are using node js,not browser,if we write this on console it will give output
+
+// in JS and React we mostly avaoid ; (semicolon)
+
+console.log(3+3)
+console.log("Reetu")
+
+
+//DataTypes Example
+let name="Reetu" //string dataType
+let age=21 //number dataType 
+let isLoggedin=false //boolean dataType
+let state;
+
+//Type of DataType
+
+//Primetive Datatypes
+// number => number has a range 2 to power of 52 that we get in memory space.
+//bigint  => rarely use in normal website , bigint is used when the number is big ,mostly used in big websites.
+//string => we use string in '' and ""  mostly used is ""
+//boolean=>true/false
+//null => standalone value , you can assign value as null,"" ,null means khali 
+//undefined => variable is declare but value is not assign eg. let state; 
+//symbol => symbol is used to find the Uniqueness 
+
+//object
+
+
+console.log(typeof age); //number
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
