c321af831e7b16ddb718c23c42f4c29bbd4531a8
Reetu Kudia <126332989+Reetu-kudia@users.noreply.github.com>
2024-11-26 11:24:27 +0000

    comparisons

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,27 @@
+// console.log(2==2);
+// console.log(2>=2);
+// console.log(2>2);
+// console.log(2!=2);
+// console.log(2<2);
+// console.log(2<=2);
+//above is the simple comparisons
+
+//comparisons of two dataTypes  //avoid this type of comparisons
+// console.log("2"==2);
+// console.log("2">=2);
+// console.log("02">2);
+
+// console.log(null==2);
+// console.log(null>2);
+// console.log(null>=2);
+// console.log(null==0);
+// console.log(null>0);
+// console.log(null>=0);
+
+// console.log(undefined==0);
+// console.log(undefined>0);
+// console.log(undefined>=0);
+
+//strict check(===) : strict check also check the dataType not only the value.
+console.log("2"==2); //this will directly do comparison
+console.log("2"===2);// this will check dataTypes
