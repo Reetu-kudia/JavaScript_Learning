cd4d3b1df592652e7097af710979fb532e2b2898
Reetu Kudia <126332989+Reetu-kudia@users.noreply.github.com>
2024-11-27 04:18:55 +0000

    comparison

--- 01_basics/04_comparisons.js
+++ 01_basics/04_comparisons.js
@@ -1,26 +1,26 @@
-// console.log(2==2);
-// console.log(2>=2);
-// console.log(2>2);
-// console.log(2!=2);
-// console.log(2<2);
-// console.log(2<=2);
+console.log(2==2);
+console.log(2>=2);
+console.log(2>2);
+console.log(2!=2);
+console.log(2<2);
+console.log(2<=2);
 //above is the simple comparisons
 
 //comparisons of two dataTypes  //avoid this type of comparisons
-// console.log("2"==2);
-// console.log("2">=2);
-// console.log("02">2);
+console.log("2"==2);
+console.log("2">=2);
+console.log("02">2);
 
-// console.log(null==2);
-// console.log(null>2);
-// console.log(null>=2);
-// console.log(null==0);
-// console.log(null>0);
-// console.log(null>=0);
+console.log(null==2);
+console.log(null>2);
+console.log(null>=2);
+console.log(null==0);
+console.log(null>0);
+console.log(null>=0);
 
-// console.log(undefined==0);
-// console.log(undefined>0);
-// console.log(undefined>=0);
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined>=0);
 
 //strict check(===) : strict check also check the dataType not only the value.
 console.log("2"==2); //this will directly do comparison
