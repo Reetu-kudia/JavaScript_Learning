a8bd11cf996b17a442d44f1e7a92e6152d2b7df7
Reetu Kudia <126332989+Reetu-kudia@users.noreply.github.com>
2024-12-13 06:05:45 +0000

    variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,6 +2,9 @@ console.log("Reetu")
 const name="Vimla"
 console.log(name)
 MobileNO=1234567890
+console.log(MobileNO)
 let accountPass="div@18"
+console.log(accountPass)
 MobileNO=222111333444
+console.log(MobileNO)
 console.table([name,MobileNO,accountPass])
