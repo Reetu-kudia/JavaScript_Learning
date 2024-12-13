d9ea3eb9072a1d0f9630f89e109f3ff04a885ad9
Reetu Kudia <126332989+Reetu-kudia@users.noreply.github.com>
2024-12-13 09:48:02 +0000

    dataType-summary

---
+++ 01_basics/05_datatype_summary.js
@@ -0,0 +1,55 @@
+//Type 1.......Primitive Datatype.....
+//There are 7 primitive data types: String,Boolean,Number,Object,Null,Symbol,BigInt
+const score=100
+const newscore=100.3
+const isLoggedin = false
+const outsidetemp=null
+let userEmail;
+
+const id=Symbol('123')
+const anotherID=Symbol('123')
+console.log(id==anotherID);
+const bigNUmber=12342334555756723434n
+
+//Type 2.......Reference Datatype/Non Primitive....... 
+//there are 3 referenvce Datatype: Array,Object,Function
+
+const superheros=["Saktiman","Spiderman","Benten","Chota bhem"] //e.g of Array
+
+//Object are in {}
+/* e.g 1
+ {  
+name:"Reetu",
+age:21,
+} */
+/* e.g 2 to store in variables
+ let myObj={  
+name:"Reetu",
+age:21,
+} */
+
+//function
+/* eg 1 declaration of function
+function()
+{
+
+ } 
+*/
+
+/* eg 2 To treate function as variables
+const MyFunstion = function()
+{
+ console.log("Hello world")
+ } 
+*/
+
+//............Types of Operators and their values................
+/*
+undefined=undefined
+Null=Object
+Boolean=Boolean
+Object=Object
+String=String
+Number=Number
+function=Function Object
+*/
