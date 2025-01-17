fcf4014a85202fc564472900bf634be4c806ca01
Reetu Kudia <126332989+Reetu-kudia@users.noreply.github.com>
2025-01-17 12:42:43 +0000

    IIFE

--- 02_basics/07_IIFE.js
+++ 02_basics/07_IIFE.js
@@ -12,20 +12,20 @@ chai()
 
 ( function chai2(){
     console.log(`DB Connected`);
-})()
+})()   // ()-this is execution call . this is used for immmediate execution.
 chai2()
 */
 
 //e.g 3 
 ( function chai3(){    //named IIFE
     console.log(`DB Connected`);
-}) ();
+}) (); //Use to end the code,it is imp neither it will give error.
 ( ()=>{       //arrow function
     console.log(`DB Connected Two`);
 })()
 
 
 //e.g 4 
-( (name)=>{
+( (name)=>{    //unnamed IIFE
     console.log(`DB Connected ${name}`);
 })('Reetu')
