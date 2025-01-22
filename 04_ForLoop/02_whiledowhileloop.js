e4fe9fdcba92f959867d128188fbc08014e73fd7
Reetu Kudia <126332989+Reetu-kudia@users.noreply.github.com>
2025-01-22 12:45:04 +0000

    While and Do while loop

---
+++ 04_ForLoop/02_whiledowhileloop.js
@@ -0,0 +1,48 @@
+//Syntax of While Loop
+/*
+while(condition){
+
+}
+*/
+
+/*
+//e.g 1
+let i=0;
+while(i<=10){
+    console.log(`Value of i is ${i}`);
+    //i++
+    i=i+2
+}
+*/
+
+/*
+//e.g 2  array
+
+let myArray=["Superman","Batman","Ironman"]
+let arr=0
+while(arr<myArray.length){
+    console.log(`Value is ${myArray[arr]}`);
+    arr++
+}
+*/
+
+//Syntax of Do While Loop 
+/*
+do{
+
+}while(condition);
+*/
+
+//e.g 1 
+let score=1
+do{
+    console.log(`Score is ${score}`);
+    score++
+}while(score<=10);
+
+//e.g 1 
+let score2=11
+do{
+    console.log(`Score is ${score2}`);
+    score2++
+}while(score2<=10);
