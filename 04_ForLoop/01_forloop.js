32171c9b331c846517c2c906d4fcac4e1fd97aa7
Reetu Kudia <126332989+Reetu-kudia@users.noreply.github.com>
2025-01-22 11:31:09 +0000

    for loops

---
+++ 04_ForLoop/01_forloop.js
@@ -0,0 +1,54 @@
+// Loops are also known as literations.
+
+//For Loop
+//e.g 1 
+for(let index=0;index<Array.length;index++){
+    const element=Array[index];
+}
+
+/*
+for(let index=0;index<10;index++){
+    const element=index;
+    console.log(element);
+}
+*/
+
+/* //e.g 3
+for(let a=0;a<=10;a++){
+    const i=a;
+    console.log(i);
+}
+*/
+
+/* //e.g 4 
+for(let i=0;i<=10;i++){
+    const a=i;
+    if(a==5){
+        console.log("5 is best number");
+    }
+    console.log(a);
+}
+
+*/
+
+/*
+//e.g 5 Printing Tables
+for(let i=1;i<=10;i++){
+    console.log("The Outer Loop is ", i )
+    for(let j=1;j<=10;j++){
+        console.log(i + '*' + j + '=' + i*j);
+    }
+}
+*/
+
+
+/*
+//e.g 6 Printing Array and its Length
+const myArray=["SpiderMan","Batman","Superman"]
+console.log(myArray.length);
+
+for(let i=0;i<myArray.length;i++){
+    const element=myArray[i];
+    console.log(element);
+}
+*/
